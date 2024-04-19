62926606df098b23fd594ed92fb9bc8f9862f326
Kyra Shi <kyrashi@usc.edu>
2024-04-19 06:48:06 +0000

    passed schedwork

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,25 +21,70 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleHelper(
+	const AvailabilityMatrix& avail,
+	const size_t dailyNeed,
+	const size_t maxShifts,
+  DailySchedule& sched,
+	size_t row, size_t col,
+	map<size_t,size_t>& shiftMap);
 
-
-// Add your implementation of schedule() and other helper functions here
-
+// Implementation
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
     const size_t maxShifts,
-    DailySchedule& sched
-)
+    DailySchedule& sched)
 {
-    if(avail.size() == 0U){
-        return false;
-    }
-    sched.clear();
-    // Add your code below
+	if(avail.size()==0U){ //empty
+		return false;
+	}
+	sched.clear(); //reset
 
-
-
-
+	size_t n = avail.size(); //rows=days
+	sched.resize(n,vector<Worker_T>()); //n by d matrix
+	map<size_t,size_t> shiftMap; //tracks each worker's shifts
+	return scheduleHelper(avail,dailyNeed,maxShifts,sched,0,0,shiftMap);
 }
 
+bool scheduleHelper(
+	const AvailabilityMatrix& avail,
+	const size_t dailyNeed,
+	const size_t maxShifts,
+  DailySchedule& sched,
+	size_t row, size_t col,
+	map<size_t,size_t>& shiftMap)
+{
+	if(row==avail.size()){ //all days are scheduled
+		return true;
+	}
+
+	if(sched[row].size()!=dailyNeed){ //more workers needed
+		while(col<avail[0].size()){
+			if(avail[row][col]==1){ //worker available
+				if(shiftMap[col]<maxShifts){ //maxShifts not reached
+					sched[row].push_back(col); //schedule worker
+					shiftMap[col]++;
+
+					bool check = scheduleHelper(avail,dailyNeed,maxShifts,sched,row,col+1,shiftMap);
+					if(check){ //done
+						return true;
+					}
+					else{ //backtrack
+						sched[row].pop_back();
+						shiftMap[col]--;
+					}
+				}
+			} 
+
+			col++; //next worker
+		}
+	}
+	
+	else{
+		//next day
+		return scheduleHelper(avail,dailyNeed,maxShifts,sched,row+1,0,shiftMap);
+	}
+
+	return false;
+}
